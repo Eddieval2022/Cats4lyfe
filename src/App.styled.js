79ad42c288c3724361408f1356795f7ac2e5c44c
Edward Valentine <eddieval2022@gmail.com>
2022-06-15 12:33:57 +0100

    Pic styling

--- src/App.styled.js
+++ src/App.styled.js
@@ -1,32 +1,33 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-	text-align: center;
+  text-align: center;
 
-	background-color: var(--color-1);
+  background-color: var(--color-1);
 
-	height: auto;
+  height: auto;
 
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-around;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 `;
 export const Content = styled.div`
-	width: calc(100vw - 100px);
+  width: calc(100vw - 100px);
 
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-around;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 `;
 
 export const Temp = styled.div`
-	margin: 50px;
-	width: 400px;
-	height: 400px;
-	border-radius: 15px;
-	background-color: var(--color-3);
-	img {
-		width: 300px;
-		height: 300px;
-	}
+  margin: 50px;
+  width: 400px;
+  height: 400px;
+  border-radius: 15px;
+  background-color: var(--color-3);
+  img {
+    width: 300px;
+    height: 300px;
+    object-fit: cover;
+  }
 `;
