ecf7c8bcc00d355bcaf14f923c8dbd2ac859dea0
alexbellairs <alexanderbellairs@gmail.com>
2022-06-15 11:52:51 +0100

    head-style

---
+++ src/components/Header.styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const HeadContainer = styled.div`
+  width: 100vw;
+  height: 130px;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+
+  background-color: var(--color-2);
+
+  h1 {
+    width: 100%;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 30%;
+  height: 54%;
+
+  button {
+    width: 30%;
+    margin-bottom: 0;
+    height: 49px;
+  }
+`;
