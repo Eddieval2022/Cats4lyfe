a37a3083b8c9e21f11dc3dba9888427df35a72e6
EvoFox <foulkes.joshua@live.co.uk>
2022-06-15 13:18:31 +0100

    created Home Page with React Router

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import {
 import Sidebar from "./components/Sidebar";
 import { useEffect, useState } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
 	const [breed, setBreed] = useState([]);
-    const [error, setError] = useState(null);
-    
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -42,25 +42,17 @@ const App = () => {
 				<HeadContainer>
 					<h1>Cats4Lyfe</h1>
 					<ButtonWrapper>
-						<button>Home</button>
-						<button>About Us</button>
-						<button>Contact Us</button>
+						<Link to="/">Home</Link>
+						<Link to="about">About Us</Link>
+						<Link to="contact">Contact Us</Link>
 					</ButtonWrapper>
 				</HeadContainer>
-				<Sidebar />
 
-				<Content>
-					{error && <p>{error}</p>}
-					{breed.slice(0, 25).map((cat) => (
-						<Temp className="info" key={cat.id}>
-							<h3>Name : {cat.name}</h3>
-							<img src={cat.image["url"]} alt="cat" />
-							<br />
-							<p>Price: £0.00</p>
-							<button>Add to cart</button>
-						</Temp>
-					))}
-				</Content>
+				<Sidebar />
+				<Routes>
+					<Route path="/" element={<Home error={error} breed={breed} />} />
+				</Routes>
+				
 			</BrowserRouter>
 		</Container>
 	);
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import { Content, Temp } from "../App.styled";
+const Home = ({ error, breed }) => {
+	return (
+		<Content>
+			{error && <p>{error}</p>}
+			{breed.slice(0, 25).map((cat) => (
+				<Temp className="info" key={cat.id}>
+					<h3>Name : {cat.name}</h3>
+					<img src={cat.image["url"]} alt="cat" />
+					<br />
+					<p>Price: £0.00</p>
+					<button>Add to cart</button>
+				</Temp>
+			))}
+		</Content>
+	);
+};
+
+export default Home;
