e61d744c57f05a9363abb72f5f0d7a061b0379cf
alexbellairs <alexanderbellairs@gmail.com>
2022-06-16 14:59:21 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import "./App.css";
 
-
 import { faker } from "@faker-js/faker";
 
 import { ButtonWrapper, HeadContainer, Container, Footer } from "./App.styled";
-
 
 import Sidebar from "./components/Sidebar";
 import { useEffect, useState } from "react";
@@ -17,71 +15,68 @@ const App = () => {
   const [breed, setBreed] = useState([]);
   const [error, setError] = useState(null);
 
+  const fetchFaker = () => {
+    const array = [];
+    for (let i = 0; i < 28; i++) {
+      const name = faker.name.findName();
+      const price = faker.commerce.price(60, 200);
+      array.push({ name, price });
+    }
+    return array;
+  };
 
-	const fetchFaker = () => {
-		const array = [];
-		for (let i = 0; i < 28; i++) {
-			const name = faker.name.findName();
-			const price = faker.commerce.price(60, 200);
-			array.push({ name, price });
-		}
-		return array;
-	};
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let fakeData = fetchFaker();
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				let fakeData = fetchFaker();
+        const response = await fetch(
+          "https://api.thecatapi.com/v1/breeds?api_key=1c98326e-5928-446a-886a-db98d650f23f"
+        );
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
 
-				const response = await fetch(
-					"https://api.thecatapi.com/v1/breeds?api_key=1c98326e-5928-446a-886a-db98d650f23f"
-				);
-				if (!response.ok) {
-					throw new Error(response.statusText);
-				}
-				const data = await response.json();
-				
-				fakeData = fakeData.map((cat, i) => {
-					cat.pics = data[i]["image"].url;
-					cat.breed = data[i].name;
-					cat.description = data[i].description;
-					cat.id = i;
+        fakeData = fakeData.map((cat, i) => {
+          cat.pics = data[i]["image"].url;
+          cat.breed = data[i].name;
+          cat.description = data[i].description;
+          cat.id = i;
 
-					console.log(cat);
-					return cat;
-				});
-				setBreed(fakeData);
-			} catch (error) {
-				console.log(error);
-				setError("could not find data");
-			}
-		};
-		fetchData();
-	}, []);
+          console.log(cat);
+          return cat;
+        });
+        setBreed(fakeData);
+      } catch (error) {
+        console.log(error);
+        setError("could not find data");
+      }
+    };
+    fetchData();
+  }, []);
 
-	return (
-		<Container>
-			<BrowserRouter>
-				<HeadContainer>
-					<h1>Cats4Lyfe</h1>
-					<ButtonWrapper>
-						<Link to="/">Home</Link>
-						<Link to="about">About Us</Link>
-						<Link to="contact">Contact Us</Link>
-					</ButtonWrapper>
-				</HeadContainer>
-				<Sidebar />
-				<Routes>
-					<Route path="/" element={<Home error={error} breed={breed} />} />
-					<Route path="about" element={<About />} />
-					<Route path="contact" element={<Contact />} />
-				</Routes>
-			</BrowserRouter>
-		</Container>
-	);
-
-
-
+  return (
+    <Container>
+      <BrowserRouter>
+        <HeadContainer>
+          <h1>Cats4Lyfe</h1>
+          <ButtonWrapper>
+            <Link to="/">Home</Link>
+            <Link to="about">About Us</Link>
+            <Link to="contact">Contact Us</Link>
+          </ButtonWrapper>
+        </HeadContainer>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home error={error} breed={breed} />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer>Copyright 2022</Footer>
+    </Container>
+  );
 };
 
 export default App;
