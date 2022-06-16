966f23844075eac4f98e553e6a90d0943a9c3152
alexbellairs <alexanderbellairs@gmail.com>
2022-06-16 12:20:34 +0100

    style changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { ButtonWrapper, HeadContainer, Container } from "./App.styled";
+import { ButtonWrapper, HeadContainer, Container, Footer } from "./App.styled";
 
 import Sidebar from "./components/Sidebar";
 import { useEffect, useState } from "react";
@@ -9,50 +9,50 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 const App = () => {
-	const [breed, setBreed] = useState([]);
-	const [error, setError] = useState(null);
+  const [breed, setBreed] = useState([]);
+  const [error, setError] = useState(null);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch(
-					"https://api.thecatapi.com/v1/breeds?api_key=1c98326e-5928-446a-886a-db98d650f23f"
-				);
-				if (!response.ok) {
-					throw new Error(response.statusText);
-				}
-				const data = await response.json();
-				console.log(data);
-				setBreed(data);
-			} catch (error) {
-				console.log(error);
-				setError("could not find data");
-			}
-		};
-		fetchData();
-	}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.thecatapi.com/v1/breeds?api_key=1c98326e-5928-446a-886a-db98d650f23f"
+        );
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        console.log(data);
+        setBreed(data);
+      } catch (error) {
+        console.log(error);
+        setError("could not find data");
+      }
+    };
+    fetchData();
+  }, []);
 
-	return (
-		<Container>
-			<BrowserRouter>
-				<HeadContainer>
-					<h1>Cats4Lyfe</h1>
-					<ButtonWrapper>
-						<Link to="/">Home</Link>
-						<Link to="about">About Us</Link>
-						<Link to="contact">Contact Us</Link>
-					</ButtonWrapper>
-				</HeadContainer>
-				<Sidebar />
-				<Routes>
-					<Route path="/" element={<Home error={error} breed={breed} />} />
-					<Route path="about" element={<About />} />
-					<Route path="contact" element={<Contact />} />
-				</Routes>
-			</BrowserRouter>
-
-		</Container>
-	);
+  return (
+    <Container>
+      <BrowserRouter>
+        <HeadContainer>
+          <h1>Cats4Lyfe</h1>
+          <ButtonWrapper>
+            <Link to="/">Home</Link>
+            <Link to="about">About Us</Link>
+            <Link to="contact">Contact Us</Link>
+          </ButtonWrapper>
+        </HeadContainer>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home error={error} breed={breed} />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer>Copyright 2022</Footer>
+    </Container>
+  );
 };
 
 export default App;
--- src/App.styled.js
+++ src/App.styled.js
@@ -12,9 +12,8 @@ export const Container = styled.div`
   justify-content: space-around;
 `;
 export const Content = styled.div`
-
-	width: calc(100vw - 100px);
-	min-height: 100vh;
+  width: calc(100vw - 100px);
+  min-height: 100vh;
 
   display: flex;
   flex-wrap: wrap;
@@ -22,58 +21,66 @@ export const Content = styled.div`
 `;
 
 export const Temp = styled.div`
+  margin: 15px auto 15px -15px;
 
-	margin: 15px auto 15px -15px;
+  width: 530px;
+  height: auto;
+  padding-bottom: 15px;
+  border-radius: 15px;
+  background-color: var(--color-3);
 
-	width: 530px;
-	height: auto;
-	padding-bottom: 15px;
-	border-radius: 15px;
-	background-color: var(--color-3);
-
-	img {
-		height: 300px;
-		width: 300px;
-	}
+  img {
+    height: 300px;
+    width: 400px;
+    object-fit: cover;
+    border-radius: 15px;
+  }
 `;
 export const HeadContainer = styled.div`
-	width: 100vw;
-	height: 132px;
+  width: 100vw;
+  height: 142px;
 
-	display: flex;
-	flex-direction: row;
-	flex-wrap: wrap;
-	align-items: center;
-	justify-content: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 40px;
 
-	background-color: var(--color-2);
+  background-color: var(--color-2);
 
-	h1 {
-		width: 100%;
-	}
+  h1 {
+    width: 100%;
+    font-size: 40px;
+  }
 `;
 
 export const ButtonWrapper = styled.div`
 	width: 30%;
-	height: 54%;
-	padding-bottom: 3px;
+	height: 20%;
+	padding-bottom: 10px;
 	a {
 		-moz-appearance: button;
 		appearance: button;
+		font-size: 25px;
 		width: 30%;
 		margin-left: 2.5px;
 		margin-right: 2.5px;
-		height: 49px;
+		height: 50px;
 		border: #000 1px solid;
+		padding: 5.5px;
+		border-radius: 10px;
 
 		text-align: center;
+		text-decoration: none;
+		color: inherit;
+		background-color: var(--color-2);
 		line-height: 300%;
 	}
 =======
   margin: 50px;
-  width: 400px;
-  height: 400px;
-  border-radius: 15px;
+  width: 100%;
+  height: 65px;
   background-color: var(--color-3);
   img {
     width: 300px;
@@ -81,4 +88,13 @@ export const ButtonWrapper = styled.div`
     object-fit: cover;
   }
 
+`;
+
+export const Footer = styled.footer`
+  font-size: 25px;
+  margin-top: 10px;
+  width: 100%;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: var(--color-6);
 `;
